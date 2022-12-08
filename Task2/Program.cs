03414444e7dbb3c043abb5fa67ce62c093f6e552
Alex <kccnbnb@mail.ru>
2022-12-08 20:22:06 +0300

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите значение x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+float count = (((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) + ((z2-z1) * (z2-z1)));
+double result = Math.Sqrt(count);
+Console.WriteLine(result);
