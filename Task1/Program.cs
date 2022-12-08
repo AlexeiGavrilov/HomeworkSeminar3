0a5f2506e3a5d3c45c1fc0094843cd3e9fff0624
Alex <kccnbnb@mail.ru>
2022-12-08 18:42:24 +0300

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int n1 = num % 10; //последняя цифра 
+int n2 = num / 10;
+int n3 = n2 % 10;// предпоследняя цифра
+int n4 = num / 10000; //первая цифра 
+int n5 = num / 1000;
+int n6 = n5 % 10;// вторая цифра 
+if (n1 == n4 && n3 == n6)
+{
+    Console.WriteLine("Ваше число: " +num + " - палиндром.");
+}
+else
+{
+    Console.WriteLine("Ваше число: " +num + " - не палиндром.");
+}
+
