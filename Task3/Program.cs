0fd3e0142f4a3f6681ccf2fb440999d7ade9194b
Alex <kccnbnb@mail.ru>
2022-12-09 12:30:58 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int[N];
+for (int i = 0; i < N; i++ )
+{
+
+    arr[i] = i*i*i;
+}
+
+void End(int[] array)
+{
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+Console.Write(array[i]+ "  ");
+}
+}
+ End(arr);
+
+
+
+
